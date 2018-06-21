b914dac2273e0f232ded56f3d15f7354785c8a7c
francofor <36070868+francofor@users.noreply.github.com>
2018-06-21 22:15:23 +0200

    Update Example_Naxos.c
    Better separation of phases

--- Example_Naxos.c
+++ Example_Naxos.c
@@ -35,14 +35,14 @@ void sendToMate(char *strIn,keyC msg,int inputByteLen)
 int main()
 {
 
-  ellipticCurve curveN;                         // chosen curve
-  keyC idA,idB,pkAx,pkAy,pkBx,pkBy;             // all keys or identifiers exchanged in byte array format
-  keyC eskA,eskB,kA,kB,Xx,Xy,Yx,Yy;             // all keys or identifiers exchanged in byte array format
-  keyC skA,skB;                                 // all internal information in coord format
+  ellipticCurve curveN;                         /* chosen curve                                           */
+  keyC idA,idB,pkAx,pkAy,pkBx,pkBy;             /* all keys or identifiers exchanged in byte array format */
+  keyC eskA,eskB,kA,kB,Xx,Xy,Yx,Yy;             /* all keys or identifiers exchanged in byte array format */
+  keyC skA,skB;                                 /* all internal information in coord format               */
 
   int i,res,z,indexC, nBytes;
 
-  srand(time(0));                               // Initialize the the standard rand() function
+  srand(time(0));                               /* Initialize the the standard rand() function            */
 
   for (z=0;z<4;z++)
   {
@@ -74,39 +74,72 @@ int main()
     clock_t start, end, startTot, endTot;
     float elapsed_time;
 
-    // Test Naxos key exchange
+    /* Test Naxos key exchange                                        */
 
-    // Phase 0
-    // Select curve. Use NIST_P224, NIST_P256, NIST_P384 or NIST_P521
+    /* Phase 0                                                        */
+    /* Select curve. Use NIST_P224, NIST_P256, NIST_P384 or NIST_P521 */
     selectCurve(&curveN,indexC);
     nBytes = (curveN.bsize+7)/8;
 
     start = clock();
     startTot = start;
 
-    // Phase 0A
-    //   The identity idA and skA should be available.
-    //   pkA should already be available before the key exchange.
-    //   Here we generate a random idA and skA for the demo
-    //   and we calculate pkA = G*skA.
-    generateRand(idA,&curveN);                // Generate idA using the rand function. To be used only for demo.
-    generateRand(skA,&curveN);                // Generate skA using the rand function. To be used only for demo.
-    // Check that skA != 0
-    publicKey(pkAx,pkAy,skA,&curveN);        // Calculate pkA from skA
+    /* Phase 0A
+         The identity idA and skA should be available.
+         pkA should already be available before the key exchange.
+         Here we generate a random idA and skA for the demo
+         and we calculate pkA = G*skA.
+	*/
+    generateRand(idA,&curveN);                /* Generate idA using the rand function. To be used only for demo. */
+    generateRand(skA,&curveN);                /* Generate skA using the rand function. To be used only for demo. */
+    if (publicKey(pkAx,pkAy,skA,&curveN)!=0)  /* Calculate pkA from skA                                          */
+    {
+      printf("Invalid skA: it is 0 or >= p\n");
+      return -1;
 
-    // Phase 1A
-    //   Generate eskA and calculate X=G*H(eskA,skA).
-    //   Done by calculateXY which also secures that H(eskA,skA) != 0
-    //   This is sent to B (it could be sent together with idA and pkA).
-    //   The type of curve should already be communicated, otherwise it could
-    //   be another info to share
-    calculateXY(Xx,Xy,eskA,skA,&curveN);
+	}
 
-    // send to B (if not already known) idA, pkAx, pkAy
+    /* Phase 0B
+         The identity idB and skB should be available.
+         pkB should already be available before the key exchange.
+         Here we generate a random idB and skB for the demo
+         and we calculate pkB = G*skB.
+	*/
+    generateRand(idB,&curveN);                /* Generate idB using the rand function. To be used only for demo. */
+    generateRand(skB,&curveN);                /* Generate skB using the rand function. To be used only for demo. */
+    if (publicKey(pkBx,pkBy,skB,&curveN)!=0)  /* Calculate pkB from skB                                          */
+    {
+      printf("Invalid skB: it is 0 or >= p\n");
+      return -1;
+
+	}
+
+    printf("PKI distributes identities and keys\n");
     sendToMate("IdA:  ",idA,nBytes);
     sendToMate("pkAx: ",pkAx,nBytes);
     sendToMate("pkAy: ",pkAy,nBytes);
-    // send to B Xx, Xy
+    printf("\n");
+    sendToMate("IdB:  ",idB,nBytes);
+    sendToMate("pkBx: ",pkBx,nBytes);
+    sendToMate("pkBy: ",pkBy,nBytes);
+    end = clock();
+    elapsed_time = (float)(end - start) / (float)CLOCKS_PER_SEC;
+    printf("Elapsed time: %f seconds\n\n", elapsed_time);
+
+    start = clock();
+
+    /* Phase 1A
+         Generate eskA and calculate X=G*H(eskA,skA).
+         Done by calculateXY which also secures that H(eskA,skA) != 0
+         This is sent to B (it could be sent together with idA and pkA).
+         The type of curve should already be communicated, otherwise it could
+         be another info to share
+	*/
+    calculateXY(Xx,Xy,eskA,skA,&curveN);
+
+
+    /* A sends Xx, Xy to B        */
+    printf("A sends X to B\n");
     sendToMate("Xx:   ",Xx,nBytes);
     sendToMate("Xy:   ",Xy,nBytes);
 
@@ -116,25 +149,16 @@ int main()
 
     start = clock();
 
-    // Phase 0B
-    //   The identity idB and skB should be available.
-    //   pkB should already be available before the key exchange.
-    //   Here we generate a random idB and skB for the demo
-    //   and we calculate pkB = G*skB.
-    generateRand(idB,&curveN);                // Generate idB using the rand function. To be used only for demo.
-    generateRand(skB,&curveN);                // Generate skB using the rand function. To be used only for demo.
-    publicKey(pkBx,pkBy,skB,&curveN);        // Calculate pkB from skB
 
-    // Phase 1B
-    //   Generate eskB and calculate Y=G*H(eskB,skB).
-    //   Done by calculateXY which also secures that H(eskB,skB) != 0
-    //   It is sent to A (it could be sent together with idB and pkB)
+    /* Phase 1B
+         Generate eskB and calculate Y=G*H(eskB,skB).
+         Done by calculateXY which also secures that H(eskB,skB) != 0
+         It is sent to A (it could be sent together with idB and pkB)
+	*/
     calculateXY(Yx,Yy,eskB,skB,&curveN);
-    // send to A (if not already known) idB, pkBx, pkBy
-    sendToMate("IdB:  ",idB,nBytes);
-    sendToMate("pkBx: ",pkBx,nBytes);
-    sendToMate("pkBy: ",pkBy,nBytes);
-    // send to A Yx, Yy
+
+    /* B sends Yx, Yy to A        */
+    printf("B sends Y to A\n");
     sendToMate("Yx:   ",Yx,nBytes);
     sendToMate("Yy:   ",Yy,nBytes);
 
@@ -144,16 +168,17 @@ int main()
 
     start = clock();
 
-    // Phase 2A
-    // A receives Y and calculates kA:
-    //     2A-0: convert pkBx and pkBy in coord format and check if it belongs to the curve
-    //     2A-1: convert Yx and Yy in coord format and checks if it belongs to the curve
-    //     2A-2: calculate Y*skA           => only coord x is used in the Hash function
-    //     2A-3: calculate pkB*H(eskA,skA) => only coord x is used in the Hash function
-    //     2A-4: calculate Y*H(eskA,skA)   => only coord x is used in the Hash function
-    //     2A-5: calculate kA = H(Y*skA, pkB*H(eskA,skA), Y*H(eskA,skA), idA, idB)
+    /* Phase 2A
+       A receives Y and calculates kA:
+           2A-0: convert pkBx and pkBy in coord format and check if it belongs to the curve
+           2A-1: convert Yx and Yy in coord format and checks if it belongs to the curve
+           2A-2: calculate Y*skA           => only coord x is used in the Hash function
+           2A-3: calculate pkB*H(eskA,skA) => only coord x is used in the Hash function
+           2A-4: calculate Y*H(eskA,skA)   => only coord x is used in the Hash function
+           2A-5: calculate kA = H(Y*skA, pkB*H(eskA,skA), Y*H(eskA,skA), idA, idB)
+	*/
 
-    res = calculateKa(kA,Yx,Yy,eskA,skA,pkBx,pkBy,idA,idB,&curveN);  // kA is returned in array of bytes
+    res = calculateKa(kA,Yx,Yy,eskA,skA,pkBx,pkBy,idA,idB,&curveN);  /* kA is returned in array of bytes */
 
     switch(res)
       {
@@ -181,8 +206,9 @@ int main()
           break;
       }
 
-    if (indexC==NIST_P521) nBytes=64;                            // we provide a key of 512 bits instead
+    if (indexC==NIST_P521) nBytes=64;                            /* we provide a key of 512 bits instead */
 
+    printf("A calculates kA\n");
     sendToMate("kA:   ",kA,nBytes);
 
     end = clock();
@@ -191,23 +217,24 @@ int main()
 
     start = clock();
 
-    // Phase 2B
-    // B receives X and calculates kB:
-    //     2B-0: convert pkAx and pkAy in coord format and check if it belongs to the curve
-    //     2B-1: convert Xx and Xy in coord format and check if it belongs to the curve
-    //     2B-2: calculate pkA*H(eskB,skB) => only coord x is used in the Hash function
-    //     2B-3: calculate X*skB           => only coord x is used in the Hash function
-    //     2B-4: calculate X*H(eskB,skB)   => only coord x is used in the Hash function
-    //     2B-5: calculate kB = H(pkA*H(eskB,skB), X*skB, X*H(eskB,skB), idA, idB)
+    /* Phase 2B
+       B receives X and calculates kB:
+           2B-0: convert pkAx and pkAy in coord format and check if it belongs to the curve
+           2B-1: convert Xx and Xy in coord format and check if it belongs to the curve
+           2B-2: calculate pkA*H(eskB,skB) => only coord x is used in the Hash function
+           2B-3: calculate X*skB           => only coord x is used in the Hash function
+           2B-4: calculate X*H(eskB,skB)   => only coord x is used in the Hash function
+           2B-5: calculate kB = H(pkA*H(eskB,skB), X*skB, X*H(eskB,skB), idA, idB)
+	*/
     res=calculateKb(kB,pkAx,pkAy,eskB,skB,Xx,Xy,idA,idB,&curveN);
     switch(res)
       {
         case -1:
-          printf("Invalid pkB: it is not mod p\n");
+          printf("Invalid pkA: it is not mod p\n");
           return -1;
 
         case -2:
-          printf("Invalid pkB: it is not on the curve\n");
+          printf("Invalid pkA: it is not on the curve\n");
           return -1;
 
         case -3:
@@ -226,6 +253,7 @@ int main()
           break;
       }
 
+    printf("B calculates kB\n");
     sendToMate("kB:   ",kB,nBytes);
 
     end = clock();
@@ -248,9 +276,16 @@ int main()
     	}
     }
 
-    if (res==1) printf("Successful, kA=kB \n");
+    if (res==1)
+    {
+    	printf("Successful, kA=kB \n");
+    }
+    else
+    {
+    	printf("Unsuccessful, kA!=kB \n");
+    }
     printf("\n\n");
   }
 
-  return 1;
+  return 0;
 }
